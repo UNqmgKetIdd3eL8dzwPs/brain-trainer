3359809cde7bc92d3c49be74a545136dfd3e73f6
bavtovich <stanislav.bavtovich@intellectsoft.net>
2016-06-11 09:37:56 +0300

    BrainTrainer.Client project added for client-server interaction

---
+++ src/BrainTrainer.Client/WebClient.cs
@@ -0,0 +1,13 @@
+﻿namespace BrainTrainer.Client
+{
+    public class WebClient
+    {
+        private const string ApiUri = @"http://db.chgk.info/xml";
+
+        private const string SearchQuestionPath = "/questions/types1";
+        private const string RandomQuestionPath = "/random";
+
+
+        
+    }
+}
