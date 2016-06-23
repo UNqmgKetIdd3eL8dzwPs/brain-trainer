a08bcf86e274685ad61bb04096de9cd15c85a96c
bavtovich <stanislav.bavtovich@intellectsoft.net>
2016-06-23 15:10:37 +0300

    QuestionView animation on swipe implemented

--- src/BrainTrainer/BrainTrainer/Pages/MainPage.xaml.cs
+++ src/BrainTrainer/BrainTrainer/Pages/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using BrainTrainer.ViewModels;
+﻿using System;
+using BrainTrainer.ViewModels;
+using Xamarin.Forms;
 
 namespace BrainTrainer.Pages
 {
@@ -8,6 +10,22 @@ namespace BrainTrainer.Pages
         {
             InitializeComponent();
             BindingContext = new MainViewModel();
+        }
+
+        private void SwipeContentView_OnSwipeLeft(object sender, EventArgs e)
+        {
+            AnimateViewOnSwipe();
+        }
+
+        private async void AnimateViewOnSwipe()
+        {
+            MainSwipeContentView.FadeTo(0, 125);
+            await MainSwipeContentView.TranslateTo(-MainSwipeContentView.Width, 0, 125);
+
+            MainSwipeContentView.TranslationX = MainSwipeContentView.Width;
+
+            MainSwipeContentView.TranslateTo(0, 0, 125);
+            await MainSwipeContentView.FadeTo(1, 125);
         }
     }
 }
