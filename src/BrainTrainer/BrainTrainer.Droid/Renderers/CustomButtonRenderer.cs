3130bad3cd31d423635dea627aee843605c10f69
Eugene Bogomolov <eugene.bogomolov@intosoft.by>
2016-06-30 12:11:10 +0300

    - added cutom fonts to buttons and label on android

---
+++ src/BrainTrainer/BrainTrainer.Droid/Renderers/CustomButtonRenderer.cs
@@ -0,0 +1,19 @@
+﻿using Android.Graphics;
+using BrainTrainer.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Button), typeof(CustomButtonRenderer))]
+namespace BrainTrainer.Droid.Renderers
+{
+    public class CustomButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+            var button = Control;
+            var font = Typeface.CreateFromAsset(Forms.Context.Assets, "roboto-condensed.regular.ttf");
+            button.Typeface = font;
+        }
+    }
+}
---
+++ src/BrainTrainer/BrainTrainer.Droid/Renderers/CustomLabelRenderer.cs
@@ -0,0 +1,21 @@
+﻿using Android.Graphics;
+using Android.Views;
+using Android.Widget;
+using BrainTrainer.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Label), typeof(CustomLabelRenderer))]
+namespace BrainTrainer.Droid.Renderers
+{
+    public class CustomLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            var label = Control;
+            var font = Typeface.CreateFromAsset(Forms.Context.Assets, "roboto-condensed.regular.ttf");
+            label.Typeface = font;
+        }
+    }
+}
